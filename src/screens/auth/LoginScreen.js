28f760e003689d47ccf55a94b5c83033bdb93ff7
UlisesSG <ukisessg@gmail.com>
2020-10-19 16:58:17 -0500

    [Add] Semantic HTML and some styles at Login

--- src/screens/auth/LoginScreen.js
+++ src/screens/auth/LoginScreen.js
@@ -1,26 +1,43 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-
+//Accesibility, handle empty password
 
 export const LoginScreen = () => {
   return (
     <div className='auth__container'>
       <h2 className='auth__title'>Login</h2>
-      <form>
-        <label className='auth__label'>
+      <p className='auth__description'>Ingresa a tu cuenta o crea una</p>
+      <form onSubmit={() => console.log('Function handle submit')}>
+        <label htmlFor='email' className='auth__label'>
           E-mail
-          <input type='text' name='email' className='auth__input'/>
-        </label>  
-        <label className='auth__label'>
+          <input
+            id='email'
+            type='email'
+            name='email'
+            placeholder='Email'
+            className='auth__input'
+          />
+        </label>
+        <label htmlFor='password' className='auth__label'>
           Password
-          <input type='password' name='password' className='auth__input'/>
-        </label>  
+          <input
+            id='password'
+            type='password'
+            name='password'
+            placeholder='Password'
+            className='auth__input'
+          />
+        </label>
 
-        <button className='auth__btn' type='submit'>Continue</button>
-      </form> 
+        <button className='auth__btn' type='submit'>
+          Continue
+        </button>
+      </form>
 
-      <Link to='/register' className='auth__link'>I do not have an account <b>Sign-Up</b></Link>
+      <Link to='/register' className='auth__link'>
+        I do not have an account <b>Sign-Up</b>
+      </Link>
     </div>
-  )
-}
+  );
+};
