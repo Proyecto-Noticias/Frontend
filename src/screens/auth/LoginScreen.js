4f775ef8406f773b2c592454107971cb16b0b38e
Ramiro Contreras León <racramiro@gmail.com>
2020-10-21 18:00:59 -0500

    [UPDATE] Styles Login

--- src/screens/auth/LoginScreen.js
+++ src/screens/auth/LoginScreen.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { startLogin } from '../../actions/auth';
-import { useForm } from '../../hooks/useForm';
+import React from "react";
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import { startLogin } from "../../actions/auth";
+import { useForm } from "../../hooks/useForm";
 
 export const LoginScreen = () => {
   const dispatch = useDispatch();
 
   const [formLoginValues, handleLoginInputChange] = useForm({
-    email: '',
-    password: '',
+    email: "",
+    password: "",
   });
 
   const { email, password } = formLoginValues;
@@ -21,46 +21,50 @@ export const LoginScreen = () => {
   };
 
   return (
-    <div className='auth__container'>
-      <h2 className='auth__title'>Login</h2>
+    <div className="auth__container">
+      <h2 className="auth__title">WELCOME TO</h2>
+      <h2 className="auth__title-second">
+        Easy<span>News</span>
+      </h2>
 
-      <p className='auth__description'>Ingresa a tu cuenta o crea una</p>
+      <p className="auth__description">Ingresa a tu cuenta o crea una</p>
       <form onSubmit={handleLogin}>
-        <label htmlFor='email' className='auth__label'>
-          E-mail
+        <label htmlFor="email" className="auth__label">
           <input
             required
-            id='email'
-            type='email'
-            name='email'
-            placeholder='Email'
-            className='auth__input'
+            id="email"
+            type="email"
+            name="email"
+            placeholder="Email"
+            className="auth__input"
             value={email}
             onChange={handleLoginInputChange}
           />
         </label>
-        <label htmlFor='password' className='auth__label'>
-          Password
+        <label htmlFor="password" className="auth__label">
           <input
             required
-            id='password'
-            type='password'
-            name='password'
-            placeholder='Password'
-            className='auth__input'
+            id="password"
+            type="password"
+            name="password"
+            placeholder="Password"
+            className="auth__input"
             value={password}
             onChange={handleLoginInputChange}
           />
         </label>
 
-        <button className='auth__btn' type='submit'>
+        <button className="auth__btn" type="submit">
           Continue
         </button>
       </form>
-
-      <Link to='/register' className='auth__link focus-style'>
-        I do not have an account <b>Sign-Up</b>
+      <p className="auth__privacity">
+        By continuing, you agree to EasyNews Terms of Service, Privacy policy.
+      </p>
+      <Link to="/register" className="auth__link focus-style">
+        Already a member? <b>Sign-Up</b>
       </Link>
     </div>
   );
 };
+// By continuing, you agree to EasyNews Terms of Service, Privacy policy.  Log in
