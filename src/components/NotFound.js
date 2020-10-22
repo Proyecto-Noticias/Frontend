52d89cac8db2dfd53cf512aded0fd1709ebdeace
Ramiro Contreras León <racramiro@gmail.com>
2020-10-22 18:28:13 -0500

    [ADD] Styles Not Found

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,12 +1,15 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import bad from "../assets/bad.png";
 
 export const NotFound = () => {
   return (
-    <div>
-      <h2>NotFound</h2>
-      <h3>Error 404</h3>
-      <Link to='/'>Go Home</Link>
+    <div className="notfound__container">
+      <img src={bad} alt="404"></img>
+      <h1 className="notfound__text">We don't find what you are looking for</h1>
+      <Link to="/">
+        <button className="notfound__btn">Go Home</button>
+      </Link>
     </div>
-  )
-}
+  );
+};
