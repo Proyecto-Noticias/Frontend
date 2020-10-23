b7102296ff85f8178472784e43d769fa65a798a2
Gabriel Zarate <gzaratet1@gmail.com>
2020-10-23 13:08:15 -0500

    [CREATED] test mock file

---
+++ src/stats-mock.js
@@ -0,0 +1,80 @@
+const stats = {
+  dailyCategories: [
+    {
+      category: 'Politic',
+      times: 500,
+    },
+    {
+      category: 'Economy',
+      times: 580,
+    },
+    {
+      category: 'Entertainment',
+      times: 620,
+    },
+    {
+      category: 'Sports',
+      times: 1000,
+    },
+    {
+      category: 'LifeStyle',
+      times: 490,
+    },
+    {
+      category: 'Culture',
+      times: 400,
+    },
+    {
+      category: 'Tecnology',
+      times: 540,
+    },
+    {
+      category: 'General',
+      times: 300,
+    },
+  ],
+  compareDaysCategories: [
+    {
+      category: 'Politic',
+      timescurrentday: 500,
+      timesyesterday: 350,
+    },
+    {
+      category: 'Economy',
+      timescurrentday: 580,
+      timesyesterday: 400,
+    },
+    {
+      category: 'Entertainment',
+      timescurrentday: 620,
+      timesyesterday: 840,
+    },
+    {
+      category: 'Sports',
+      timescurrentday: 1000,
+      timesyesterday: 990,
+    },
+    {
+      category: 'LifeStyle',
+      timescurrentday: 490,
+      timesyesterday: 510,
+    },
+    {
+      category: 'Culture',
+      timescurrentday: 400,
+      timesyesterday: 380,
+    },
+    {
+      category: 'Tecnology',
+      timescurrentday: 540,
+      timesyesterday: 650,
+    },
+    {
+      category: 'General',
+      timescurrentday: 300,
+      timesyesterday: 250,
+    },
+  ],
+};
+
+export default stats;
