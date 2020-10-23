ecc4b98806a33d9c7b8046455b93d178b6285877
Ramiro Contreras León <racramiro@gmail.com>
2020-10-22 19:35:49 -0500

    [UPDATE] HTML and CSS Home-NewSection

--- src/components/news/NewsSection.js
+++ src/components/news/NewsSection.js
@@ -1,63 +1,89 @@
-import React from 'react';
+import React from "react";
+import { useSelector } from "react-redux";
+
+import SkipLink from "../shared/SkipLink";
+
+const paths = [
+  {
+    key: 1,
+    path: "#main",
+    name: "Saltar al contenido principal",
+  },
+];
 
 const NewsSection = () => {
+  const { name } = useSelector((state) => state.auth);
+
   return (
-    <section id='categories' className='sections__container'>
-      <h1 className='sections__container--title'>
-        Read<span>By</span>
-      </h1>
-      <div className='sections__container--items l-flex-wrap'>
-        <nav>
-          <ul className='sections__list'>
-            <li>
-              <a
-                title='Noticias de politica'
-                className='sections__list--item focus-style'
-                href='#x'
-              >
-                Politic
-              </a>
-            </li>
-            <li>
-              <a
-                title='Noticias de deportes'
-                className='sections__list--item focus-style'
-                href='#x'
-              >
-                Sports
-              </a>
-            </li>
-            <li>
-              <a
-                title='Noticias de self care'
-                className='sections__list--item focus-style'
-                href='#x'
-              >
-                Self Care
-              </a>
-            </li>
-            <li>
-              <a
-                title='Noticias de economía'
-                className='sections__list--item focus-style'
-                href='#x'
-              >
-                Economy
-              </a>
-            </li>
-            <li>
-              <a
-                title='Noticias de entretenimiento'
-                className='sections__list--item focus-style'
-                href='#x'
-              >
-                Entertaiment
-              </a>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </section>
+    <>
+      <SkipLink paths={paths} />
+      <section id="categories" className="newsection__container">
+        <div className="newsection__container-text">
+          <h1 className="newsection__title-user">¡Hola {name}!</h1>
+          <label className="newsection__label">
+            <input
+              required
+              type="text"
+              name="search"
+              placeholder="Search"
+              className="newsection__input"
+            />
+          </label>
+        </div>
+        <div>
+          {/* <h1 className="sections__container--title">
+            Read<span>By</span>
+          </h1> */}
+          <div className="sections__container--items l-flex-wrap">
+            <nav>
+              <ul className="sections__list">
+                <li>
+                  <a
+                    title="Noticias de politica"
+                    className="sections__list--item focus-style"
+                    href="#x">
+                    Politic
+                  </a>
+                </li>
+                <li>
+                  <a
+                    title="Noticias de deportes"
+                    className="sections__list--item focus-style"
+                    href="#x">
+                    Sports
+                  </a>
+                </li>
+
+                <li>
+                  <a
+                    title="Noticias de self care"
+                    className="sections__list--item focus-style"
+                    href="#x">
+                    Self Care
+                  </a>
+                </li>
+                <li>
+                  <a
+                    title="Noticias de economía"
+                    className="sections__list--item focus-style"
+                    href="#x">
+                    Economy
+                  </a>
+                </li>
+                <li>
+                  <a
+                    title="Noticias de entretenimiento"
+                    className="sections__list--item focus-style"
+                    href="#x">
+                    Entertaiment
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { useSelector } from "react-redux";
-
 import { Link } from "react-router-dom";
+
+import { useSelector } from "react-redux";
 import SkipLink from "./SkipLink";
 
 const paths = [
@@ -26,9 +26,9 @@ const Navbar = () => {
                 Always<span>Update</span>
               </Link>
             </li>
-            <li>
+            {/* <li>
               <p className="header__title-user">{name}</p>
-            </li>
+            </li> */}
             <li className="login__container-navbar">
               <Link to="/login" className="header__title-right">
                 <button className="header__title-right-button" type="submit">
