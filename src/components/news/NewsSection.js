d5225009ad95f3a605521a67aabff1890bb9de1b
UlisesSG <ukisessg@gmail.com>
2020-10-23 16:27:08 -0500

    [Add] Accesibility at Home Screen

--- src/components/news/NewsSection.js
+++ src/components/news/NewsSection.js
@@ -1,14 +1,14 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
-import SkipLink from "../shared/SkipLink";
+import SkipLink from '../shared/SkipLink';
 
 const paths = [
   {
     key: 1,
-    path: "#main",
-    name: "Saltar al contenido principal",
+    path: '#main',
+    name: 'Saltar al contenido principal',
   },
 ];
 
@@ -18,16 +18,16 @@ const NewsSection = () => {
   return (
     <>
       <SkipLink paths={paths} />
-      <section id="categories" className="newsection__container">
-        <div className="newsection__container-text">
-          <h1 className="newsection__title-user">¡Hola {name}!</h1>
-          <label className="newsection__label">
+      <section id='categories' className='newsection__container'>
+        <div className='newsection__container-text'>
+          <h1 className='newsection__title-user'>¡Hola {name}!</h1>
+          <label className='newsection__label'>
             <input
               required
-              type="text"
-              name="search"
-              placeholder="Search"
-              className="newsection__input"
+              type='text'
+              name='search'
+              placeholder='Search'
+              className='newsection__input'
             />
           </label>
         </div>
@@ -35,62 +35,69 @@ const NewsSection = () => {
           {/* <h1 className="sections__container--title">
             Read<span>By</span>
           </h1> */}
-          <div className="sections__container--items ">
+          <div className='sections__container--items '>
             <nav>
-              <ul className="sections__list">
+              <ul className='sections__list'>
                 <li>
                   <Link
-                    title="Noticias de politica"
-                    className="sections__list--item"
-                    to="category/politics">
+                    title='Noticias de politica'
+                    className='sections__list--item'
+                    to='category/politics'
+                  >
                     Politic
                   </Link>
                 </li>
                 <li>
                   <Link
-                    title="Noticias de deportes"
-                    className="sections__list--item"
-                    to="category/sports">
+                    title='Noticias de deportes'
+                    className='sections__list--item'
+                    to='category/sports'
+                  >
                     Sports
                   </Link>
                 </li>
                 <li>
                   <Link
-                    title="Noticias de self care"
-                    className="sections__list--item"
-                    to="category/health-lifestyle">
+                    title='Noticias de self care'
+                    className='sections__list--item'
+                    to='category/health-lifestyle'
+                  >
                     Lifestyle
                   </Link>
                 </li>
                 <li>
                   <Link
-                    title="Noticias de economía"
-                    className="sections__list--item"
-                    to="category/economy">
+                    title='Noticias de economía'
+                    className='sections__list--item'
+                    to='category/economy'
+                  >
                     Economy
                   </Link>
                 </li>
                 <li>
                   <Link
-                    title="Noticias de entretenimiento"
-                    className="sections__list--item"
-                    to="category/entertaiment">
+                    title='Noticias de entretenimiento'
+                    className='sections__list--item'
+                    to='category/entertaiment'
+                  >
                     Entertaiment
                   </Link>
                 </li>
                 <li>
                   <Link
-                    title="Noticias de entretenimiento"
-                    className="sections__list--item"
-                    to="category/culture">
+                    title='Noticias de entretenimiento'
+                    className='sections__list--item'
+                    to='category/culture'
+                  >
                     Culture
                   </Link>
                 </li>
                 <li>
                   <Link
-                    title="Noticias de entretenimiento"
-                    className="sections__list--item"
-                    to="category/technology">
+                    title='Noticias de entretenimiento'
+                    className='sections__list--item'
+                    to='category/technology'
+                  >
                     Tech
                   </Link>
                 </li>
