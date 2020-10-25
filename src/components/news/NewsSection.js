614e2b7c8100db7634673d6e750bd59404a8c4a2
UlisesSG <ukisessg@gmail.com>
2020-10-25 01:34:01 -0500

    [Fix] Skip Link

--- src/components/news/NewsSection.js
+++ src/components/news/NewsSection.js
@@ -1,16 +1,6 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import { Link, useParams } from "react-router-dom";
-
-import SkipLink from "../shared/SkipLink";
-
-const paths = [
-  {
-    key: 1,
-    path: "#main",
-    name: "Saltar al contenido principal",
-  },
-];
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Link, useParams } from 'react-router-dom';
 
 const NewsSection = () => {
   const { name } = useSelector((state) => state.auth);
@@ -18,90 +8,98 @@ const NewsSection = () => {
 
   return (
     <div>
-      <SkipLink paths={paths} />
-      <section id="categories" className="newsection__container">
-        <div className="newsection__container-text">
-          <h1 className="newsection__title-user">¡Hello {name}!</h1>
-          <label className="newsection__label">
+      <section id='categories' className='newsection__container'>
+        <div className='newsection__container-text'>
+          <h1 className='newsection__title-user'>¡Hello {name}!</h1>
+          <label className='newsection__label'>
             <input
+              id='search'
               required
-              type="text"
-              name="search"
-              placeholder="Search"
-              className="newsection__input"
+              title='Search news'
+              type='text'
+              name='search'
+              placeholder='Search news'
+              className='newsection__input'
             />
           </label>
         </div>
-        <div className="sections__main">
+        <div className='sections__main'>
           {/* <h1 className="sections__container--title">
             Read<span>By</span>
           </h1> */}
-          <div className="sections__container--items ">
+          <div className='sections__container--items '>
             <nav>
-              <ul className="sections__list">
+              <ul className='sections__list'>
                 <li>
                   <Link
-                    title="Noticias de politica"
-                    className="sections__list--item"
-                    to={isActive.category === "politics" ? "#" : "/politics"}>
+                    title='Noticias de politica'
+                    className='sections__list--item'
+                    to={isActive.category === 'politics' ? '#' : '/politics'}
+                  >
                     Politic
                   </Link>
                 </li>
                 <li>
                   <Link
-                    title="Noticias de deportes"
-                    className="sections__list--item"
-                    to={isActive.category === "sports" ? "#" : "/sports"}>
+                    title='Noticias de deportes'
+                    className='sections__list--item'
+                    to={isActive.category === 'sports' ? '#' : '/sports'}
+                  >
                     Sports
                   </Link>
                 </li>
                 <li>
                   <Link
-                    title="Noticias de self care"
-                    className="sections__list--item"
+                    title='Noticias de self care'
+                    className='sections__list--item'
                     to={
-                      isActive.category === "health-lifestyle"
-                        ? "#"
-                        : "/health-lifestyle"
-                    }>
+                      isActive.category === 'health-lifestyle'
+                        ? '#'
+                        : '/health-lifestyle'
+                    }
+                  >
                     Lifestyle
                   </Link>
                 </li>
                 <li>
                   <Link
-                    title="Noticias de economía"
-                    className="sections__list--item"
-                    to={isActive.category === "economy" ? "#" : "/economy"}>
+                    title='Noticias de economía'
+                    className='sections__list--item'
+                    to={isActive.category === 'economy' ? '#' : '/economy'}
+                  >
                     Economy
                   </Link>
                 </li>
                 <li>
                   <Link
-                    title="Noticias de entretenimiento"
-                    className="sections__list--item"
+                    title='Noticias de entretenimiento'
+                    className='sections__list--item'
                     to={
-                      isActive.category === "entertainment"
-                        ? "#"
-                        : "/entertainment"
-                    }>
+                      isActive.category === 'entertainment'
+                        ? '#'
+                        : '/entertainment'
+                    }
+                  >
                     Entertainment
                   </Link>
                 </li>
                 <li>
                   <Link
-                    title="Noticias de entretenimiento"
-                    className="sections__list--item"
-                    to={isActive.category === "culture" ? "#" : "/culture"}>
+                    title='Noticias de entretenimiento'
+                    className='sections__list--item'
+                    to={isActive.category === 'culture' ? '#' : '/culture'}
+                  >
                     Culture
                   </Link>
                 </li>
                 <li>
                   <Link
-                    title="Noticias de entretenimiento"
-                    className="sections__list--item"
+                    title='Noticias de entretenimiento'
+                    className='sections__list--item'
                     to={
-                      isActive.category === "technology" ? "#" : "/technology"
-                    }>
+                      isActive.category === 'technology' ? '#' : '/technology'
+                    }
+                  >
                     Tech
                   </Link>
                 </li>
