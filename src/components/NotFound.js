cd4667e155051c1bddf148ea506a985b1b8bc72a
UlisesSG <ukisessg@gmail.com>
2020-10-25 01:45:40 -0500

    [Update] Lazy loading at App Router

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,15 +1,15 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import bad from "../assets/bad.png";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import bad from '../assets/bad.png';
 
-export const NotFound = () => {
+export default function NotFound() {
   return (
-    <div className="notfound__container">
-      <img src={bad} alt="error 404"></img>
-      <h1 className="notfound__text">We don't find what you are looking for</h1>
-      <Link to="/">
-        <button className="notfound__btn">Go Home</button>
+    <div className='notfound__container'>
+      <img src={bad} alt='error 404'></img>
+      <h1 className='notfound__text'>We don't find what you are looking for</h1>
+      <Link to='/'>
+        <button className='notfound__btn'>Go Home</button>
       </Link>
     </div>
   );
-};
+}
