291b249292a0ee5fd177b3520fd309102a2c45a2
Juan Rios <mrdaniel19996@gmail.com>
2020-10-26 10:07:04 -0500

    [UPDATE]Footer with link to blog fixed

--- src/components/shared/Footer.js
+++ src/components/shared/Footer.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 
 const Footer = () => {
   return (
     <div className="footer__container">
       <div>Made with love by MALKOMEDIA</div>
+      <Link to='/blog'>
+        Blog
+      </Link>
     </div>
   );
 };
