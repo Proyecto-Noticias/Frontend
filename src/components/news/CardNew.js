727c3a36a65215f7c6b769a2be16901b9af06027
UlisesSG <ukisessg@gmail.com>
2020-10-25 20:20:00 -0600

    [ADD] Compressed images

--- src/components/news/CardNew.js
+++ src/components/news/CardNew.js
@@ -1,29 +1,29 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import noImage from "../../assets/notphoto.png";
-import cara from "../../assets/bad.png";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import noImage from '../../assets/notphoto.jpg';
+import cara from '../../assets/bad.png';
 //Add an aria-role at Link
 // focus - style;
 const CardNew = ({ imageUrl, title, _id }) => {
   return (
-    <Link className="focus-article" title={title} to={`/new/${_id}`}>
-      <article className={"card__container"}>
-        <div className="card__container-image">
+    <Link className='focus-article' title={title} to={`/new/${_id}`}>
+      <article className={'card__container'}>
+        <div className='card__container-image'>
           <img
             src={imageUrl || noImage}
-            alt="Portada de la noticia"
-            loading="lazy"
-            className="card__container-img"
+            alt='Portada de la noticia'
+            loading='lazy'
+            className='card__container-img'
           />
           <img
-            className="card__container-sentiment"
+            className='card__container-sentiment'
             src={cara}
-            alt="Sensitive calification"
-            loading="lazy"
+            alt='Sensitive calification'
+            loading='lazy'
           />
         </div>
-        <div className="card__items">
-          <h2 className="card__items--label">{title}</h2>
+        <div className='card__items'>
+          <h2 className='card__items--label'>{title}</h2>
         </div>
       </article>
     </Link>
