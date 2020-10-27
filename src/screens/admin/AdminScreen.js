b5e919080a9b8753595df88291299f9149a045b6
Gabriel Zarate <gzaratet1@gmail.com>
2020-10-27 07:53:10 -0600

    [ADD] GraphicsCategory component

--- src/screens/admin/AdminScreen.js
+++ src/screens/admin/AdminScreen.js
@@ -2,8 +2,8 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { statsCategoryLoaded } from '../../actions/stats';
 import { adminUsersLoaded } from '../../actions/admin';
+import GraphicsCategory from '../../components/admin/GraphicsCategory';
 import AdminTable from '../../components/admin/AdminTable';
-import StatNewsAreaChart from '../../components/statsNews/StatNewsAreaChart';
 
 const AdminScreen = () => {
   const dispatch = useDispatch();
@@ -23,15 +23,7 @@ const AdminScreen = () => {
     <div className='admin__container'>
       <h1>Console Admin</h1>
       <AdminTable />
-      <div>
-        <StatNewsAreaChart data={categories} />
-        {/* <StatNewsSimpleBarChart data={categories} />
-        <StatNewsPieChart data={categories} /> */}
-        {/* <StatNewsAreaChart data={stats.dailyCategories} />
-        <StatNewsSimpleBarChart data={stats.compareDaysCategories} />
-        <StatNewsStackedBarChart data={stats.compareDaysCategories} />
-        <StatNewsPieChart data={stats.dailyCategories} /> */}
-      </div>
+      <GraphicsCategory categories={categories} />
     </div>
   );
 };
