25d173be035fadd313e04399b845365c1ab6aba1
Ulisessg <ukisessg@gmail.com>
2020-10-27 05:09:16 -0600

    [Fix] Unnecessary tabs in header

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -1,38 +1,38 @@
-import React, { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
-import { Link, useHistory, useLocation } from "react-router-dom";
-import { LogoutBtn } from "../LogoutBtn";
-import SkipLink from "./SkipLink";
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { Link, useHistory, useLocation } from 'react-router-dom';
+import { LogoutBtn } from '../LogoutBtn';
+import SkipLink from './SkipLink';
 
 const paths = [
   {
     key: 1,
-    path: "#main",
-    name: "Skip to main content",
+    path: '#main',
+    name: 'Skip to main content',
   },
 ];
 
 const homePaths = [
   {
     key: 1,
-    path: "#main",
-    name: "Skip to main content",
+    path: '#main',
+    name: 'Skip to main content',
   },
   {
     key: 2,
-    path: "#search",
-    name: "Skip to search news",
+    path: '#search',
+    name: 'Skip to search news',
   },
   {
     key: 3,
-    path: "#news",
-    name: "Skip to news",
+    path: '#news',
+    name: 'Skip to news',
   },
 ];
 
 const Navbar = () => {
   const { checking } = useSelector((state) => state.auth);
-  const [currentPath, setCurrentPath] = useState("");
+  const [currentPath, setCurrentPath] = useState('');
   const history = useHistory();
   const actualLocation = useLocation();
   useEffect(() => {
@@ -43,51 +43,44 @@ const Navbar = () => {
 
   return (
     <>
-      <SkipLink paths={currentPath !== "/" ? paths : homePaths} />
+      <SkipLink paths={currentPath !== '/' ? paths : homePaths} />
       <header>
-        <div className="header__container l-flex-header">
-          <ul className="header-title__container l-flex-header">
+        <div className='header__container l-flex-header'>
+          <ul className='header-title__container l-flex-header'>
             <li>
-              <Link to="/" className="home">
+              <Link to='/' className='home'>
                 Always<span>Update</span>
               </Link>
             </li>
-            <li className="login__container-navbar">
-            
-            {
-              (actualLocation.pathname === '/login' ) 
-              ?    
-              <a href="#authlogin" className="header__title-right">
-                {checking ? (
-                  <button className="header__title-right-button" type="submit">
-                    Log in
-                  </button>
+            <span className='login__container-navbar'>
+              <li className='header--item'>
+                {actualLocation.pathname === '/login' ? (
+                  <div className='header__title-left'>
+                    {checking ? (
+                      <Link className='header__title-left'>Log in</Link>
+                    ) : (
+                      <LogoutBtn />
+                    )}
+                  </div>
+                ) : checking ? (
+                  <div className='header__title-left'>
+                    <Link to='/login' className='header__title-left'>
+                      Login
+                    </Link>
+                  </div>
                 ) : (
                   <LogoutBtn />
                 )}
-              </a>          
-              :
-              <Link to="/login" className="header__title-right">
-                {checking ? (
-                  <button className="header__title-right-button" type="submit">
-                    Log in
-                  </button>
-                ) : (
-                  <LogoutBtn />
-                )}
-              </Link>
-              
+              </li>
 
-            }
-
-              
-
-
-
-              <Link to="/register" className="header__title-right">
-                {checking ? <div>Sing up</div> : null}
-              </Link>
-            </li>
+              <li>
+                <div className='header__title-rigth'>
+                  <Link className='header__title-right' to='/register'>
+                    {checking ? 'Sing up' : null}
+                  </Link>
+                </div>
+              </li>
+            </span>
           </ul>
         </div>
       </header>
