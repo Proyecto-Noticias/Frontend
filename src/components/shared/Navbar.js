0265428de24c073889f3aed63247d53d3d76e5db
Ramiro Contreras León <racramiro@gmail.com>
2020-10-27 11:44:45 -0500

    [UPDATE] BlogScreen

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -1,37 +1,36 @@
-import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
-import { Link, useHistory, useLocation } from 'react-router-dom';
-import { LogoutBtn } from '../LogoutBtn';
-import SkipLink from './SkipLink';
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { Link, useHistory, useLocation } from "react-router-dom";
+import { LogoutBtn } from "../LogoutBtn";
+import SkipLink from "./SkipLink";
 
 const paths = [
   {
     key: 1,
-    path: '#main',
-    name: 'Skip to main content',
+    path: "#main",
+    name: "Skip to main content",
   },
 ];
 
 const homePaths = [
   {
     key: 1,
-    path: '#main',
-    name: 'Skip to main content',
+    path: "#main",
+    name: "Skip to main content",
   },
   {
     key: 2,
-    path: '#search',
-    name: 'Skip to search news',
+    path: "#search",
+    name: "Skip to search news",
   },
   {
     key: 3,
-    path: '#news',
-    name: 'Skip to news',
+    path: "#news",
+    name: "Skip to news",
   },
 ];
 
 const Navbar = () => {
-
   const { checking, isAdmin } = useSelector((state) => state.auth);
   const [currentPath, setCurrentPath] = useState("");
 
@@ -45,58 +44,56 @@ const Navbar = () => {
 
   return (
     <>
-      <SkipLink paths={currentPath !== '/' ? paths : homePaths} />
+      <SkipLink paths={currentPath !== "/" ? paths : homePaths} />
       <header>
-        <div className='header__container l-flex-header'>
-          <ul className='header-title__container l-flex-header'>
+        <div className="header__container l-flex-header">
+          <ul className="header-title__container l-flex-header">
             <li>
-              <Link to='/' className='home'>
+              <Link to="/" className="home">
                 Always<span>Update</span>
               </Link>
             </li>
 
             {isAdmin && (
-              <li className='header__statistics'>
-                <Link to='/stats'>Go Admin</Link>
+              <li className="header__statistics">
+                <Link to="/stats">Go Admin</Link>
               </li>
             )}
             <li className="login__container-navbar">
-            
-            {
-              (actualLocation.pathname === '/login' ) 
-              ?    
-              <a href="#authlogin" className="header__title-right">
-                {checking ? (
-                  <button className="header__title-right-button" type="submit">
-                    Log in
-                  </button>
-                ) : (
-                  <LogoutBtn />
-                )}
-              </a>          
-              :
-              <Link to="/login" className="header__title-right">
-                {checking ? (
-                  <button className='header__title-right-button' type='submit'>
-                    Log in
-                  </button>
-                ) : (
-                  <LogoutBtn />
-                )}
-              </Link>
-              
-
-            }
-
+              {actualLocation.pathname === "/login" ? (
+                <a href="#authlogin" className="header__title-right">
+                  {checking ? (
+                    <button
+                      className="header__title-right-button"
+                      type="submit">
+                      Log in
+                    </button>
+                  ) : (
+                    <LogoutBtn />
+                  )}
+                </a>
+              ) : (
+                <Link to="/login" className="header__title-right">
+                  {checking ? (
+                    <button
+                      className="header__title-right-button"
+                      type="submit">
+                      Log in
+                    </button>
+                  ) : (
+                    <LogoutBtn />
+                  )}
+                </Link>
+              )}
 
               <li>
-                <div className='header__title-rigth'>
-                  <Link className='header__title-right' to='/register'>
-                    {checking ? 'Sing up' : null}
+                <div className="header__title-rigth">
+                  <Link className="header__title-right" to="/register">
+                    {checking ? "Sing up" : null}
                   </Link>
                 </div>
               </li>
-            </span>
+            </li>
           </ul>
         </div>
       </header>
