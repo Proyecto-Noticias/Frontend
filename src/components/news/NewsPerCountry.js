eb6239a85b1818486f246c1a32f382573b6d1b31
Juan Rios <mrdaniel19996@gmail.com>
2020-10-28 11:41:40 -0500

    [UPDATE]News per country only if user

--- src/components/news/NewsPerCountry.js
+++ src/components/news/NewsPerCountry.js
@@ -1,11 +1,17 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { newStartCountryLoading } from "../../actions/news";
 import CardNew from "./CardNew";
 
 export const NewsPerCountry = () => {
-
+  const dispatch = useDispatch()
   const {newsCountry} = useSelector(state => state.news)
+  const { country } = useSelector(state => state.auth)
   
+
+  useEffect(() => {
+    dispatch ( newStartCountryLoading(country.toLowerCase()) )
+  }, [dispatch, country]) 
 
   return (
     <div className="recomendation__container">
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,28 +1,26 @@
-import React, { memo, useEffect } from 'react';
+import React, { memo } from 'react';
 
 import NewsSection from '../components/news/NewsSection';
 import NewsGrid from '../components/news/NewsGrid';
 import { UserSearch } from '../components/news/UserSearch';
-import { useDispatch, useSelector } from 'react-redux';
+import {  useSelector } from 'react-redux';
 import { NewsPerCountry } from '../components/news/NewsPerCountry';
-import { newStartCountryLoading } from '../actions/news';
+
 
 const HomeScreen = () => {
-  const dispatch = useDispatch()
   const { searchNews } = useSelector((state) => state.news)
+  const { checking } = useSelector((state) => state.auth)
   const { newsArraySearched } = searchNews
-  const { country } = useSelector(state => state.auth)
   
-
-  useEffect(() => {
-    dispatch ( newStartCountryLoading(country.toLowerCase()) )
-  }, [dispatch, country])
   
   return (
     <>
       <NewsSection />
       {newsArraySearched.length > 0 ? <UserSearch /> : <NewsGrid />}
-      <NewsPerCountry />
+      
+      {
+        (!checking) && <NewsPerCountry />
+      }
     </>
   );
 };
