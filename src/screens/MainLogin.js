0fb5548d105e1005b589f3e9fb989290f2ba5414
Ramiro Contreras León <racramiro@gmail.com>
2020-10-28 00:25:44 -0500

    [UPDATE] confirmation CSS

--- src/screens/MainLogin.js
+++ src/screens/MainLogin.js
@@ -2,13 +2,11 @@ import React, { memo } from "react";
 
 import LoginScreen from "./auth/LoginScreen";
 import MainTitle from "../components/MainTitle";
-import Confirmation from "../components/Confirmation";
 const MainLogin = () => {
   return (
     <>
       <div className="mainlogin__background">
         <MainTitle />
-        <Confirmation />
         <LoginScreen />
       </div>
     </>
