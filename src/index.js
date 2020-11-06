da0c8207ffe155de581e1cc997db7db320e8abde
Facundo García Martoni <43690718+facmartoni@users.noreply.github.com>
2020-11-06 19:25:05 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+///// START CODE REVIEW COMMENT
+
+// Excellent code!
+
+///// END CODE REVIEW COMMENT
+
 import React from "react";
 import ReactDOM from "react-dom";
 import AlwaysUpdate from "./AlwaysUpdate";
